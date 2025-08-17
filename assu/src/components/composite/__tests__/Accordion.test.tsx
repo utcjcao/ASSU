9b8612cd079224c100cb84c8f34c3538640119a0
Rashu <shardarashu@gmail.com>
2025-08-17 16:51:10 +0530

    Fixed test import

--- assu/src/components/composite/__tests__/Accordion.test.tsx
+++ assu/src/components/composite/__tests__/Accordion.test.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom";
-import StyledAccordion from "./Accordion";
+import StyledAccordion from "../Accordion";
 
 const mockItems = [
   {
