a9699f8f5be855c96720c2142db8dac312ad97e0
Shirley <shirley.yao.zhang@gmail.com>
2025-08-19 04:34:11 -0400

    moved Button.tsx

--- assu/components/common/__tests__/Button.test.tsx
+++ assu/components/common/__tests__/Button.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import Button from "components/Button";
+import Button from "components/common/Button";
 
 describe("Button", () => {
   it("renders a button with the given text", () => {
