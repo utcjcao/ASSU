cffaaf3431e0bda66b9654c1c83858bbdd512cb4
utcjcao <chrisj.cao@mail.utoronto.ca>
2025-08-23 20:28:59 -0400

    removed contradicting css tag

--- assu/components/ui/accordion.tsx
+++ assu/components/ui/accordion.tsx
@@ -45,7 +45,7 @@ function AccordionTrigger({
       <AccordionPrimitive.Trigger
         data-slot="accordion-trigger"
         className={cn(
-          "text-[var(--color-title)] font-[var(--font-body-medium)] py-5 hover:no-underline focus-visible:border-ring focus-visible:ring-ring/50 flex flex-1 items-start justify-between gap-4 rounded-md text-left text-sm transition-all outline-none hover:underline focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 [&[data-state=open]>svg]:rotate-180",
+          "text-[var(--color-title)] font-[var(--font-body-medium)] py-5 focus-visible:border-ring focus-visible:ring-ring/50 flex flex-1 items-start justify-between gap-4 rounded-md text-left text-sm transition-all outline-none hover:underline focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 [&[data-state=open]>svg]:rotate-180",
           className
         )}
         {...props}
