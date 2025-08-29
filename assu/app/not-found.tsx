5fcd6da1f7563c33c4849651bfbf3577958b8234
Stanley Pang <stanley.pang@mail.utoronto.ca>
2025-08-29 00:22:22 -0400

    escape single quotes in not-found.tsx

--- assu/app/not-found.tsx
+++ assu/app/not-found.tsx
@@ -31,8 +31,8 @@ export default function NotFound() {
       </Text>
 
       <Text as="p" className="text-lg mb-8 max-w-md" color="secondary">
-        Sorry, we couldn't find the page you're looking for. It might have been
-        moved, deleted, or you entered the wrong URL.
+        Sorry, we couldn&apos;t find the page you&apos;re looking for. It
+        might have been moved, deleted, or you entered the wrong URL.
       </Text>
 
       {/* Navigation Options */}
