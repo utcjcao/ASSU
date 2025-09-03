34dc0422a7d50d9a1f7ec22edeca9e522edb7184
utcjcao <chrisj.cao@mail.utoronto.ca>
2025-09-03 16:32:31 -0400

    removed assu root folder

--- components/common/__tests__/Divider.test.tsx
+++ components/common/__tests__/Divider.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import Divider from "../../common/Divider";
+import Divider from "../Divider";
 
 describe("Divider", () => {
   it("renders divider element", () => {
